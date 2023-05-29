33d93894e976739c3d16178b6ce0fc949a6b6fd6
adityamaulana <aditmau26@gmail.com>
2023-05-29 11:04:49 +0700

    seventh comm

--- SolvedMultiWaris_040/SolvedMultiWaris_040/SolvedMultiWaris_040.cpp
+++ SolvedMultiWaris_040/SolvedMultiWaris_040/SolvedMultiWaris_040.cpp
@@ -11,4 +11,15 @@ class People{
 			cout <<"People 12 years old maked" << Age << "\n" << endl;
 		}
 
-}
+};
+
+class Worker : virtual public People
+{
+	public:
+
+		Worker(int pAge) :
+			People(pAge)
+		{
+			cout << "Worker Maked\n" << endl;
+		}
+};
