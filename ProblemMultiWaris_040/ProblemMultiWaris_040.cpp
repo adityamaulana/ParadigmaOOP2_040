9ed2d5d2824093ff64d31a98700c72f6ed5e26ad
adityamaulana <aditmau26@gmail.com>
2023-05-29 10:51:44 +0700

    Add project files.

---
+++ ProblemMultiWaris_040/ProblemMultiWaris_040.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class People
+{
+public:
+	int Age;
+
+	People (int pAge) :
+		Age(pAge)
+	{
+		cout << "People 12 years old maked" << Age << "\n" << endl;
+	}
+};
+
+class Worker : public People
+{
+public:
+	
+};
